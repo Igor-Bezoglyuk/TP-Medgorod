281fe5fec922c2ac719bf7c2d134478c6d1a8afa
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2022-03-20 04:56:32 +0300

    new

--- TP Medgorod/js/script.js
+++ TP Medgorod/js/script.js
@@ -3,5 +3,8 @@ jQuery( document ).ready(function(){
     $(".Open-Close").click(function(){
     $(".Header-nav-phone").slideToggle(500);
  });
+    $(window).resize(function(){
+  $(".Header-nav-phone").hide();
+});    
 });
 });
